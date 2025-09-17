cf3e981d3f46879e59e6184100edc1d93eff5eab
Pisfil <ronny_pm@hotmail.com>
2025-09-17 17:27:33 -0500

    feat:subiendo ejercicio poo

--- 12.poo.py
+++ 12.poo.py
@@ -16,3 +16,20 @@ productos = [Producto(nombre_producto="Manzana Fuji",
 
 for p in productos:
     print(f"{p.nombre} cuesta S/ {p.precio} y tiene un stock {p.stock}")
+
+
+class Usuario:
+    def __init__(self, nombre, email):
+        self.nombre = nombre
+        self.email = email
+        self.estado = False
+
+    def estado_usuario(self):
+        return 'Activo' if self.estado == True else "Inactivo"
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}\nEmail: {self.email}\nEstado: {self.estado_usuario()}"
+
+
+usuario_1 = Usuario("Ronny", 'Ronny_pm@hotmail.com')
+print(usuario_1)
